e62ff746a1d4d7f4e79af541ed672d9c4a111dca
Riley Farro <rileyfarro@gmail.com>
2021-03-06 17:23:25 +0800

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+A
+gradle.properties,2\a\2afbb999f001938c88fa43fc2ef52abf0f8213e4
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+q
+Aapp/src/main/java/com/codinginflow/imagesearchapp/MainActivity.kt,7\c\7ced8677da83af076033a93dac1831ad61703e77
+r
+Bapp/src/main/java/com/codinginflow/imagesearchapp/UnsplashPhoto.kt,5\1\51d1227e4385f81d299bcb2f97afc44afc6b2b11
---
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/UnsplashPhotoAdapter.kt
@@ -0,0 +1,57 @@
+package com.codinginflow.imagesearchapp.ui.gallery
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.codinginflow.imagesearchapp.R
+import com.codinginflow.imagesearchapp.data.UnsplashPhoto
+import com.codinginflow.imagesearchapp.databinding.ItemUnsplashPhotoBinding
+
+class UnsplashPhotoAdapter :
+    PagingDataAdapter<UnsplashPhoto, UnsplashPhotoAdapter.ViewHolder>(PHOTO_COMPARATOR) {
+
+    companion object {
+        private val PHOTO_COMPARATOR = object : DiffUtil.ItemCallback<UnsplashPhoto>() {
+            override fun areItemsTheSame(oldItem: UnsplashPhoto, newItem: UnsplashPhoto) =
+                oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: UnsplashPhoto, newItem: UnsplashPhoto) =
+                oldItem == newItem
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = getItem(position)
+
+        if(currentItem != null) {
+            holder.bind(currentItem)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            ItemUnsplashPhotoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    class ViewHolder(private val binding: ItemUnsplashPhotoBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(photo: UnsplashPhoto) {
+            binding.apply {
+                Glide.with(itemView)
+                    .load(photo.urls.regular)
+                    .centerCrop()
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .error(R.drawable.ic_error)
+                    .into(imageView)
+
+                textViewUsername.text = photo.user.username
+            }
+        }
+    }
+}
